a2559789295e9c3115b9882351fbc340f3f641a3
Federico <federicoarenas1246@gmail.com>
2021-03-11 19:25:10 +0000

    .

--- DeepLabV3Plus/experiments.sh
+++ DeepLabV3Plus/experiments.sh
@@ -1,1 +1,1 @@
-python main.py --model deeplabv3plus_resnet50 --gpu_id 0 --year 2012_aug  --lr 0.01 --crop_size 512 --batch_size 16 --output_stride 16 --dataset 'agr' --num_classes 7 --save_val_results --val_interval 10
+python main.py --model deeplabv3plus_resnet50 --gpu_id 0 --year 2012_aug  --lr 0.01 --crop_size 512 --batch_size 16 --output_stride 16 --dataset 'agr' --num_classes 7 --save_val_results --val_interval 100
